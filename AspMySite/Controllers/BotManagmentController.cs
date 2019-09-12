6b5f18b9f779d38d4d32bd6d98c6913c770ac987
Dmitry <43281522+KartonDCP@users.noreply.github.com>
2019-09-12 16:27:04 +0300

    Add files via upload

---
+++ AspMySite/Controllers/BotManagmentController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AspMySite.Controllers
+{
+    public class BotManagmentController : Controller
+    {
+        // GET: BotManagment
+        public ActionResult BotManagementIndex()
+        {
+            return View();
+        }
+    }
+}
