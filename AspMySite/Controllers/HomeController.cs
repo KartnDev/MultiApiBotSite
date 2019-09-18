df40e08c7b5f053f230853c74f6163634a0c4deb
Dmitry <43281522+KartonDCP@users.noreply.github.com>
2019-09-18 23:49:39 +0300

    Add files via upload

--- AspMySite/Controllers/HomeController.cs
+++ AspMySite/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using AspMySite.Models.BotModel.BotActioner.VkMethods;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -21,7 +22,9 @@ namespace AspMySite.Controllers
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
-
+            //VkMethods methods = new VkMethods("e3c30bf8a99dd17da33d10b5087627fd5b23ce60cb6e894d4598d8265b4ea7310b85ed6fabb5da3019b37");
+            //methods.Kick(257606382, 3);
+            //methods.Invite(257606382, 3);
             return View();
         }
 
